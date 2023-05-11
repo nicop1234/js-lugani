5f8ad0bfd8e9b887d30486b6644a6f04ba5d5784
nicop1234 <marcelonicolaspereyra@irem.edu.ar>
2023-05-11 10:35:03 -0300

    comit

---
+++ actividad2/bucles-for.js
@@ -0,0 +1,23 @@
+let acum = 1
+let acum2 = 1
+let acum3 = 1
+let cont = 1
+
+//bucle for
+console.log("bucle for")
+for (let i = 0; i < 11; i++) {
+acum = acum * (i+1)
+console.log(acum)
+}
+
+//bucle while
+console.log("bucle while")
+while (cont < 11) {
+    acum2 = acum2 * (cont+1)
+    console.log(acum2)
+    cont=+1
+  }
+
+
+
+
