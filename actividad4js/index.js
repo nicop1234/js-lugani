80eb9d8c0ab738b1c13e685d12825f2b8a269c48
nicop1234 <marcelonicolaspereyra@irem.edu.ar>
2023-05-18 08:48:12 -0300

    act4

---
+++ actividad4js/index.js
@@ -0,0 +1,9 @@
+let alturaCentimetros = 170
+let alturaMetros = 1.69
+let pesoKilo = 78.100
+
+console.log("altura en centimetros: " +alturaCentimetros)
+console.log("altura en metros: " +alturaMetros)
+console.log("peso: " +pesoKilo)
+console.log("altura en metros redondeada: " + Math.round(alturaMetros))
+console.log("peso redondeado: " + Math.round(pesoKilo))
